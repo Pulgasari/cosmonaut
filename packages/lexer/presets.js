3d2f2fdc32ea0d9f7f8af0b1661e0f1d751f7ae4
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-11 04:02:46 +0200

    Update presets.js

--- packages/lexer/presets.js
+++ packages/lexer/presets.js
@@ -1,5 +1,12 @@
 // @cosmonaut/lexer/presets.js
 
+export const puncts = '()[]{}?!.:,;';
+export const tokens = ['COMMENT','EOF','IDENTIFIER','KEYWORD','NUMBER','OPERATOR','PUNCT','STRING'];
+
+export const TokenType     = Object.freeze(Object.fromEntries(tokens.map(k => [k,k])));
+export const TokenTypeList = Object.freeze(tokens);
+export const TokenTypeSet  = new Set(TokenTypeList);
+
 export const      cStyleComments = [{ type: 'line', start: '//' }, { type: 'block', start: '/*', end: '*/' }];
 export const   hashStyleComments = [{ type: 'line', start: '#' }];
 export const   dashStyleComments = [{ type: 'line', start: '--' }];
