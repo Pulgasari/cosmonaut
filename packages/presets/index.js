26524f5f2a18064b1496c83426289d1f8a657f91
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-12 22:44:41 +0200

    Update index.js

--- packages/presets/index.js
+++ packages/presets/index.js
@@ -1,1 +1,34 @@
-// @cosmonaut/presets/index.js
+// @cosmonaut/presets
+
+// :::::: Comment Styles
+
+export const commonStyleComments = [
+  { type: 'line', start: '//' },
+  { type: 'line', start: '#' },
+  { type: 'block', start: '/*', end: '*/' },
+];
+export const cStyleComments = [
+  { type: 'line', start: '//' }, 
+  { type: 'block', start: '/*', end: '*/' }
+];
+export const hashStyleComments = [
+  { type: 'line', start: '#' }
+];
+export const dashStyleComments = [
+  { type: 'line', start: '--' }
+];
+export const pythonStyleComments = [
+  { type: 'line', start: '#' },
+  { type: 'block', start: "'''", end: "'''" },
+  { type: 'block', start: '"""', end: '"""' },
+];
+
+// :::::: Basic Content Rules
+// rules: { use: [baseRules.doubleQuoteString, baseRules.number, baseRules.identifier], override: { number: myOwnNumberRule } }
+
+export const baseRules = {
+  doubleQuoteString : { id: 'doubleQuoteString', type: 'STRING',     regex: /"(?:\\.|[^"\\])*"/        },
+  singleQuoteString : { id: 'singleQuoteString', type: 'STRING',     regex: /'(?:\\.|[^'\\])*'/        },
+  number            : { id: 'number',            type: 'NUMBER',     regex: /\d+(?:\.\d+)?/            },
+  identifier        : { id: 'identifier',        type: 'IDENTIFIER', regex: /[a-zA-Z_$][a-zA-Z0-9_$]*/ },
+};
