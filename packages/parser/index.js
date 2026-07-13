bb7843fed493357d467b6e8365855273a1ed7aaa
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-13 04:47:02 +0200

    Update index.js

--- packages/parser/index.js
+++ packages/parser/index.js
@@ -1,1 +1,66 @@
 // @cosmonaut/parser
+
+const defaultOptions = {
+  grammar     : [],   // Module mit parseXxx-Regeln, s.u.
+  keywords    : [],
+  nodeFactory : null, // s.u. (ASTNode-Proxy)
+  puncts      : [],
+  operators   : {},
+  tokenTypes  : null, // z.B. via buildTokenTypes() aus @cosmonaut/lexer
+  wrappers    : {},   // custom open/close-Paare, s.u.
+};
+
+export class Parser {
+  constructor (tokens = [], options = {}) {
+    this.options = mergeOptions(options, defaultOptions);
+    if (!this.options.nodeFactory) throw new Error('[Parser] options.nodeFactory fehlt.');
+
+    this._tokenMap = buildTokenValueMap(this.options);
+    this._wrappers = buildWrapperMap(this.options.wrappers);
+    this.node      = this.options.nodeFactory;
+    this._stack    = [];
+    
+    this.setTokens(tokens);
+    this._buildDispatch();
+  }
+
+  // navigation
+  advance  () { if (!this.isEOF()) this.cursor++; return this.previous(); }
+  peek     () { return this.tokens[this.cursor]; }
+  peekNext () { return this.tokens[this.cursor + 1]; }
+  previous () { return this.tokens[this.cursor - 1]; }
+  isEOF    () { return this.isToken('EOF'); }
+
+  // matching
+  isToken      (typeOrValue, maybeValue) { /* wie bisher, aber via this._tokenMap */ }
+  matchToken   (typeOrValue, maybeValue) { if (this.isToken(typeOrValue, maybeValue)) { this.advance(); return true; } return false; }
+  consumeToken (typeOrValue, maybeValue, message) { /* wie bisher, this.error(...) */ }
+
+  error (message) {
+    const t = this.peek();
+    return new SyntaxError(`[Parser ${t.line}:${t.column}]: ${message} (Gefunden: '${t.value}')`);
+  }
+
+  // Sub-Parsing auf anderem Token-Stream (Template-Expr, JSX-Islands, ...)
+  setTokens (tokens) { this.tokens = tokens; this.cursor = 0; }
+
+  withTokens (tokens, fn) {
+    this._stack.push({ tokens: this.tokens, cursor: this.cursor });
+    this.setTokens(tokens);
+    try { return fn(); }
+    finally { ({ tokens: this.tokens, cursor: this.cursor } = this._stack.pop()); }
+  }
+
+  // from utils
+  parseList             (...args) { return parseList             (this, ...args); } // parseElement, options
+  parseWrapped          (...args) { return parseWrapped          (this, ...args); } // wrapper, fn
+  parseUntil            (...args) { return parseUntil            (this, ...args); } // parseElement, stop
+  parseBinaryExpression (...args) { return parseBinaryExpression (this, ...args); } // options, min
+
+  // internal
+  _buildDispatch () { /* s.u. Punkt 4 */ }
+  
+}
+
+default export Parser;
+
