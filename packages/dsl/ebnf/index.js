dc83f23b6cb2f2a7e0272a484013eda49666b0fd
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-15 01:47:28 +0200

    Update index.js

--- packages/dsl/ebnf/index.js
+++ packages/dsl/ebnf/index.js
@@ -1,16 +1,20 @@
 // EBNF
 
-import { tokenizeEBNF } from './tokenizer.js';
-import { parseEBNF }    from './parser.js';
-import { compileEBNF }  from './compiler.js'; // (noch nicht gezeigt)
+export tokenize  from './tokenize.js';
+export toAST     from './toAST.js';
+export toMethods from './toMethods.js';
 
-export default function installEBNF (Parser, grammarSource) {
-  const tokens  = tokenizeEBNF (grammarSource); // tokenize :: convert grammar into tokens
-  const ast     =    parseEBNF (tokens);        // parse    :: convert tokens  into ast
-  const methods =  compileEBNF (ast);           // compile  :: convert ast     into parser methods
-  
-  Parser.addMethod(methods);
+export function convert ({ from, to, input }) {
+  if (from === 'grammar' && to === 'tokens')
+  if (from === 'grammar' && to === 'ast)
+  if (from === 'grammar' && to === 'methods)
+  if (from === 'tokens'  && to === 'ast)
+  if (from === 'tokens'  && to === 'methods)
+  if (from === 'ast'     && to === 'methods)
 }
+
+const EBNF = { tokenize, toAST, toMethods };
+export default EBNF;
 
 // :::::: TOKEN TYPES
 
