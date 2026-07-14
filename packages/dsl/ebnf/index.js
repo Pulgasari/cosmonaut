30e879fc16f8cc60ddf6822480ce04544f57bff5
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-15 00:55:30 +0200

    Update index.js

--- packages/dsl/ebnf/index.js
+++ packages/dsl/ebnf/index.js
@@ -11,3 +11,15 @@ export default function installEBNF (Parser, grammarSource) {
   
   Parser.addMethod(methods);
 }
+
+// :::::: TOKEN TYPES
+
+export const EBNF_TOKEN_TYPES = {
+  IDENTIFIER: 'IDENTIFIER',   // z.B. "function-declaration"
+  STRING:     'STRING',       // "async" oder ';'
+  SYMBOL:     'SYMBOL',       // ::=, |, [, ], {, }, ?, :, (, ), ...
+  ELLIPSIS:   'ELLIPSIS',     // ...
+  REGEX:      'REGEX',        // ? any valid js regexp pattern ?
+  COMMENT:    'COMMENT',      // # Kommentar
+  EOF:        'EOF',
+};
