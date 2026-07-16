53267f91a21b1df726597af64c6ec196a5612c3f
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-17 01:17:41 +0200

    Update index2.js

--- packages/lexer/index2.js
+++ packages/lexer/index2.js
@@ -1,10 +1,29 @@
+export function makeRulesFromStringList (stringList, tokenType) {
+  return ensureArray(stringList).map(
+    value => ({ id: `${type}:${value}`, type, value, regex: makeStickyRegex(RegExp.escape(String(value))) })
+  );
+}
+
 
 import Cursor   from '@cosmonaut/cursor';
 import Streamer from '@cosmonaut/streamer';
+import {
+  makeRulesFromStringList,
+};
 
 export const Tokenizer {
   
-  constructor (inputCode) {
+  constructor (inputCode, config = {
+    operators : [],
+    puncts    : [],
+  }) {
+    let { operators, puncts } = config;
+    if (config.operators) {
+      makeRulesFromStringList (operators, 'OPERATOR');
+    }
+    if (config.operators) {
+      makeRulesFromStringList (puncts, 'PUNCT);
+    }
     this.inputCode = inputCode;
     this.rules     = [];
     this.tokens    = [];
