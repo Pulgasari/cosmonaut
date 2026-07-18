b287b2a2fb0e1d630baac5748f49ddef702ba3f5
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-18 09:42:13 +0200

    Update repeat.js

--- packages/parse/blocks/repeat.js
+++ packages/parse/blocks/repeat.js
@@ -82,6 +82,78 @@ export const sepBy = (item, separator) => decorate(parser => {
   return results;
 });
 
+export const sepBy1 = (item, separator) => decorate(parser => {
+  const first = item(parser);
+  if (first == null) return null;
+  return [ first, ...sepBy(item, separator)(parser) ];
+});
 
+export const sepEndBy = (item, separator) => decorate(parser => {
+  const results = [];
+  const first = backtrack(parser, item);
 
+  if (first == null) return results;
+  results.push(first);
 
+  while (true) {
+    if (backtrack(parser, separator) == null) break;
+
+    const next = backtrack(parser, item);
+    if (next == null) break; // trailing separator allowed
+    results.push(next);
+  }
+  return results;
+});
+
+export const sepEndBy1 = (item, separator) => decorate(parser => {
+  const first = item(parser);
+  if (first == null) return null;
+  return [ first, ...sepEndBy(item, separator)(parser) ];
+});
+
+export const manyTill = (item, end) => decorate(parser => {
+
+    const results = [];
+
+    while (true) {
+
+        if (backtrack(parser, end) != null) {
+            end(parser);
+            break;
+        }
+
+        const pos = parser.save();
+
+        const result = item(parser);
+
+        if (result == null) {
+            parser.restore(pos);
+            return null;
+        }
+
+        if (parser.index === pos) {
+            throw new Error("manyTill(): parser consumed no input.");
+        }
+
+        results.push(result);
+
+    }
+
+    return results;
+
+});
+
+export const many1Till = (item, end) => decorate(parser => {
+
+    const first = item(parser);
+
+    if (first == null) {
+        return null;
+    }
+
+    return [
+        first,
+        ...manyTill(item, end)(parser)
+    ];
+
+});
