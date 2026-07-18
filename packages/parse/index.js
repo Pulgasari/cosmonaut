5f9e5e80b7f7fa9f08f2a73c63cb031a1ca9cc10
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-18 11:54:28 +0200

    Update index.js

--- packages/parse/index.js
+++ packages/parse/index.js
@@ -1,9 +1,20 @@
 // @cosmonaut/parser
 
 // :::::: Classes
+
 export Parser      from './classes/Parser.js';
 export ParserState from './classes/ParserState.js';
 
 // :::::: The Blocks
+
 export * from './blocks/atoms.js';
 export * from './blocks/flow.js';
+export * from './blocks/repeat.js';
+export * from './blocks/transform.js';
+
+// :::::: The Methods
+
+export { default as parseBinaryExpr  } from './methods/parseBinaryExpr.js';
+export { default as parseListPattern } from './methods/parseListPattern.js';   
+export { default as parsePattern     } from './methods/parsePattern.js';
+export { default as parseUnaryExpr   } from './methods/parseUnaryExpr.js';
