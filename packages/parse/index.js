d32a64a147429a0f72daa48f202ecaef2da1ee5f
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-18 09:23:53 +0200

    Create index.js

---
+++ packages/parse/index.js
@@ -0,0 +1,9 @@
+// @cosmonaut/parser
+
+// :::::: Classes
+export Parser      from './classes/Parser.js';
+export ParserState from './classes/ParserState.js';
+
+// :::::: The Blocks
+export * from './blocks/atoms.js';
+export * from './blocks/flow.js';
