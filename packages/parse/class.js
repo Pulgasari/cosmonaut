18437138e503a3f2c246c99f66c6bc45ff0b1cf5
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-18 09:07:32 +0200

    Add Parser class for token parsing functionality

---
+++ packages/parse/class.js
@@ -0,0 +1,62 @@
+export default class Parser {
+
+    constructor(tokens = []) {
+        this.setTokens(tokens);
+    }
+
+    setTokens(tokens) {
+        this.tokens = tokens;
+        this.index = 0;
+    }
+
+    reset() {
+        this.index = 0;
+    }
+
+    peek(offset = 0) {
+        return this.tokens[this.index + offset] ?? null;
+    }
+
+    next() {
+        if (!this.eof()) this.index++;
+        return this.peek(-1);
+    }
+
+    eof() {
+        return this.peek()?.type === "EOF";
+    }
+
+    save() {
+        return this.index;
+    }
+
+    restore(position) {
+        this.index = position;
+    }
+
+    check(typeOrValue) {
+        const token = this.peek();
+        if (!token) return false;
+
+        return token.type === typeOrValue
+            || token.value === typeOrValue;
+    }
+
+    match(typeOrValue) {
+        if (!this.check(typeOrValue)) return null;
+        return this.next();
+    }
+
+    consume(typeOrValue) {
+        const token = this.match(typeOrValue);
+
+        if (token == null) {
+            throw new SyntaxError(
+                `Expected '${typeOrValue}'`
+            );
+        }
+
+        return token;
+    }
+
+}
