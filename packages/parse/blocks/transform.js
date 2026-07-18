3061167b807f3fbdd3bbecabb07deebd967fe181
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-18 09:47:50 +0200

    Create transform.js

---
+++ packages/parse/blocks/transform.js
@@ -0,0 +1,60 @@
+export const map = (parser, fn) => decorate(state => {
+
+    const result = parser(state);
+
+    return result == null
+        ? null
+        : fn(result, state);
+
+});
+
+export const value = (parser, value) => decorate(state => {
+
+    const result = parser(state);
+
+    return result == null
+        ? null
+        : value;
+
+});
+
+export const capture = (parser, name) => decorate(state => {
+
+    const result = parser(state);
+
+    if (result == null) {
+        return null;
+    }
+
+    return {
+        [name]: result
+    };
+
+});
+
+export const tap = (parser, fn) => decorate(state => {
+
+    const result = parser(state);
+
+    if (result != null) {
+        fn(result, state);
+    }
+
+    return result;
+
+});
+
+export const filter = (parser, predicate) => decorate(state => {
+
+    const result = parser(state);
+
+    if (result == null) {
+        return null;
+    }
+
+    return predicate(result, state)
+        ? result
+        : null;
+
+});
+
