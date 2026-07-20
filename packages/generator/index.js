1bd19b97fea635d4ebf31839707168068dc7c721
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-20 11:01:42 +0200

    Update index.js

--- packages/generator/index.js
+++ packages/generator/index.js
@@ -1,1 +1,17 @@
-// @cosmonaut/generator/index.js
+// @cosmonaut/generator
+
+// :::::: Doc (Library) Re-Exports
+
+export * from '@cosmonaut/doc';
+export { print } from '@cosmonaut/doc-printer';
+
+// :::::: Classes
+
+export { default }              from './classes/Generator.js';
+export { default as Generator } from './classes/Generator.js';
+
+// :::::: Methods
+
+export { default as genBinaryExpr } from './methods/genBinaryExpr.js';
+export { default as genList       } from './methods/genList.js';
+export { default as genUnaryExpr  } from './methods/genUnaryExpr.js';
