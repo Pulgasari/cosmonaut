992c2f63e3d8c953669fe62b5eebe3f36410af06
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-25 03:03:28 +0200

    Create imports.js

---
+++ docs/_includes/js/imports.js
@@ -0,0 +1,6 @@
+// docs/_includes/js/imports.js
+
+import { h, render } from 'https://cdn.jsdelivr.net/npm/preact@10.19.3/+esm';
+import { signal }    from 'https://cdn.jsdelivr.net/npm/@preact/signals-core@1.5.0/+esm';
+import htm           from 'https://cdn.jsdelivr.net/npm/htm@3.1.1/+esm'; const html = htm.bind(h);
+import hljs          from 'https://cdn.jsdelivr.net/npm/highlight.js@11.9.0/+esm';
