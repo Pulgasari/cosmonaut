3a4df1dd74f996c791e8172e5a75fef5a57618c8
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-26 23:49:50 +0200

    Update index.js

--- packages/presets/index.js
+++ packages/presets/index.js
@@ -1,45 +1,9 @@
 // @cosmonaut/presets
 
-// :::::: Comment Styles
-
-export const commonStyleComments = [
-  { type: 'line', start: '//' },
-  { type: 'line', start: '#' },
-  { type: 'block', start: '/*', end: '*/' },
-];
-export const cStyleComments = [
-  { type: 'line', start: '//' }, 
-  { type: 'block', start: '/*', end: '*/' }
-];
-export const hashStyleComments = [
-  { type: 'line', start: '#' }
-];
-export const dashStyleComments = [
-  { type: 'line', start: '--' }
-];
-export const pythonStyleComments = [
-  { type: 'line', start: '#' },
-  { type: 'block', start: "'''", end: "'''" },
-  { type: 'block', start: '"""', end: '"""' },
-];
-
-// :::::: Basic Content Rules
-// rules: { use: [baseRules.doubleQuoteString, baseRules.number, baseRules.identifier], override: { number: myOwnNumberRule } }
-
-export const baseRules = {
-  doubleQuoteString : { id: 'doubleQuoteString', type: 'STRING',     regex: /"(?:\\.|[^"\\])*"/        },
-  singleQuoteString : { id: 'singleQuoteString', type: 'STRING',     regex: /'(?:\\.|[^'\\])*'/        },
-  number            : { id: 'number',            type: 'NUMBER',     regex: /\d+(?:\.\d+)?/            },
-  identifier        : { id: 'identifier',        type: 'IDENTIFIER', regex: /[a-zA-Z_$][a-zA-Z0-9_$]*/ },
-};
-
-// :::::: Common
-
-export const commonPuncts = '()[]{}?!.:,;';
-export const commonTokens = ['COMMENT','EOF','IDENTIFIER','KEYWORD','NUMBER','OPERATOR','PUNCT','STRING'];
 
 // :::::: Re-Export of Language Presets
 
+export * from './commons.js';
 export { default as javascript } from './languages/javascript.js';
 
 
