16ddff8da038b7fe390a8cc5d62a0300dc6694ca
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-26 17:11:17 +0200

    Update transform.js

--- packages/parser/blocks/transform.js
+++ packages/parser/blocks/transform.js
@@ -1,29 +1,31 @@
 // @cosmonaut/blocks/parser/transform.js
 
+import { decorate } from './_internals.js';
+
 export const
 
 capture = (parser, name) => decorate (state => {
   const result = parser(state);
-  return result == null ? null : {[name]: result};
+  return result === undefined ? undefined : {[name]: result};
 }),
 
 filter = (parser, predicate) => decorate (state => {
   const result = parser(state);
-  return result == null ? null : predicate(result, state) ? result : null;
+  return result === undefined ? undefined : predicate(result, state) ? result : undefined;
 }),
 
 map = (parser, fn) => decorate (state => {
   const result = parser(state);
-  return result == null ? null : fn(result, state);
+  return result === undefined ? undefined : fn(result, state);
 }),
 
 tap = (parser, fn) => decorate (state => {
   const result = parser(state);
-  if (result != null) fn(result, state);
+  if (result !== undefined) fn(result, state);
   return result;
 }),
 
 value = (parser, value) => decorate (state => {
   const result = parser(state);
-  return result == null ? null : value;
+  return result === undefined ? undefined : value;
 });
