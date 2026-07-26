76da40d955cfe5f207742bc1544a38bbdb53f12c
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-26 23:32:22 +0200

    Update javascript.js

--- packages/presets/languages/javascript.js
+++ packages/presets/languages/javascript.js
@@ -127,7 +127,7 @@ export const operators = {
 
 export const puncts = ['{', '}', '(', ')', '[', ']', ',', ';', '.', ':', '?'];
 
-default export {
+export default {
   builtins,
   comments,
   globals,
