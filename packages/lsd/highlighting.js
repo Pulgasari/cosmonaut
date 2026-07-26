2a111a4642b7df44fa9bce84f84696e20cf2d2e7
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-27 00:07:49 +0200

    Update highlighting.js

--- packages/lsd/highlighting.js
+++ packages/lsd/highlighting.js
@@ -4,7 +4,8 @@ export function parseHighlighting (hlLines) {
   const scopes = {};
 
   for (const line of hlLines) {
-    const match = line.match(/^HL\s+(\S+)\s*==\s*`([^`]+)`$/);
+    const match = line.match(/^HL\s+(\S+)\s*==\s*(.+)$/);
+    //const match = line.match(/^HL\s+(\S+)\s*==\s*`([^`]+)`$/);
     if (!match) throw new Error(`[lsd] Malformed HL line: "${line}"`);
     const [, tokenName, scope] = match;
     scopes[tokenName] = scope;
