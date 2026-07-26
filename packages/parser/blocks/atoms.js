8909f58ced18f1fb43efc9f2bb42b325244d1c96
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-26 17:06:38 +0200

    Update atoms.js

--- packages/parser/blocks/atoms.js
+++ packages/parser/blocks/atoms.js
@@ -6,7 +6,7 @@ export const
 check   = value => decorate (parser => parser.check  (value)),
 expect  = value => decorate (parser => parser.consume(value)),
 token   = value => decorate (parser => parser.match  (value)),
-any     = ()    => decorate (parser => parser.eof() ? null : parser.next()),
-eof     = ()    => decorate (parser => parser.eof() ? true : null),
+any     = ()    => decorate (parser => parser.eof() ? undefined : parser.next()),
+eof     = ()    => decorate (parser => parser.eof() ? true : undefined),
 succeed = value => decorate (parser => value),
-fail    = ()    => decorate (parser => null);
+fail    = ()    => decorate (parser => undefined);
