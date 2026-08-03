84b0521fedf0838e960d76d3e2fc34711569e908
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-03 16:16:03 +0200

    Create parseUnaryExpr.js

---
+++ packages/cosmonaut/patterns/parseUnaryExpr.js
@@ -0,0 +1,26 @@
+// @cosmonaut/cosmonaut/patterns/parseUnaryExpr.js
+
+export default function (parser, config) {
+
+  const {
+    operators,
+    parseOperand,
+    buildNode,
+    specialCases = []
+  } = config;
+
+  for (const special of specialCases) {
+    if (special.test(parser)) return special.parse(parser);
+  }
+
+  for (const [token, operator] of Object.entries(operators)) {
+    if (!parser.check(token)) continue;
+
+    parser.advance();
+    const argument = parseUnaryExpr (parser, config);
+    return buildNode (operator, argument);
+  }
+
+  return parseOperand();
+
+}
