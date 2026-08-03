cf1a7276f25929d34f4522c0d885d27beb9870fe
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-03 16:07:35 +0200

    Create index.js

---
+++ packages/cosmonaut/presets/index.js
@@ -0,0 +1,15 @@
+// @cosmonaut/cosmonaut/presets
+
+
+// :::::: Re-Export of Language Presets
+
+export * from './commons.js';
+//export { default as javascript } from './languages/javascript.js';
+
+
+
+/*
+export const TokenType     = Object.freeze(Object.fromEntries(tokens.map(k => [k,k])));
+export const TokenTypeList = Object.freeze(tokens);
+export const TokenTypeSet  = new Set(TokenTypeList);
+*/
