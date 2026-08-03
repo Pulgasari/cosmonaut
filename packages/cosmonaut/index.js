8f3164f0c170f010a50b7240158be3ec7ae161d9
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-03 15:51:47 +0200

    Create index.js

---
+++ packages/cosmonaut/index.js
@@ -0,0 +1,31 @@
+// @cosmonaut/cosmonaut
+
+// The compiler toolkit. Everything a language implementation needs, in one
+// package: the three stages, the machine they share, the streams they read,
+// and the reusable grammar/codegen patterns.
+
+// :::::: Main Export
+
+export { default }              from './Compiler.js';
+export { default as Cosmonaut } from './Compiler.js';
+
+// :::::: Stages
+
+export { default as Lexer }     from './stages/Lexer.js';
+export { default as Parser }    from './stages/Parser.js';
+export { default as Generator } from './stages/Generator.js';
+
+// :::::: Machine + Streams
+
+export { default as Machine }     from './machine/Machine.js';
+export { default as TokenStream } from './streams/TokenStream.js';
+export { default as CharStream }  from './streams/CharStream.js';
+
+// :::::: Toolkit Re-Exports
+
+export * as layouter from '@cosmonaut/layouter';
+export * as parsers  from '@cosmonaut/parsers';
+
+// :::::: Utils
+
+export * from './utils/index.js';
