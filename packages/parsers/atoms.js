ece62568d3a1d892dd38d296b64f5ed436d90002
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-03 15:32:46 +0200

    Create atoms.js

---
+++ packages/parsers/atoms.js
@@ -0,0 +1,18 @@
+// @cosmonaut/parsers/atoms.js
+
+// The only blocks that touch the stream's token-level API directly.
+// Everything else in this package is built out of these plus each other.
+
+import { decorate } from './_internal.js';
+
+export const
+
+check   = value => decorate (stream => stream.check  (value)),
+expect  = value => decorate (stream => stream.consume(value)),
+token   = value => decorate (stream => stream.match  (value)),
+
+any     = ()    => decorate (stream => stream.eof() ? undefined : stream.next()),
+eof     = ()    => decorate (stream => stream.eof() ? true : undefined),
+
+succeed = value => decorate (stream => value),
+fail    = ()    => decorate (stream => undefined);
