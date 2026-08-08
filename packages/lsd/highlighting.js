96131c034932f53994586d056a03ed122cdce28c
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-08 21:05:50 +0200

    Create highlighting.js

---
+++ packages/lsd/highlighting.js
@@ -0,0 +1,27 @@
+// @cosmonaut/lsd/highlighting.js
+
+export function parseHighlighting (hlLines) {
+  const scopes = {};
+
+  for (const line of hlLines) {
+    //const match = line.match(/^HL\s+(\S+)\s*==\s*(.+)$/);
+    //const match = line.match(/^HL\s*::\s*(\S+)\s*==\s*(.+)$/);
+    const match = line.match(/^HL\s*::\s*(\S+)\s*==\s*`([^`]+)`$/);
+    //const match = line.match(/^HL\s+(\S+)\s*==\s*`([^`]+)`$/);
+    if (!match) throw new Error(`[lsd] Malformed HL line: "${line}"`);
+    const [, tokenName, scope] = match;
+    scopes[tokenName] = scope;
+  }
+
+  return scopes;
+}
+
+/*
+?????
+
+// vorher:
+function extractLiteralPrefix (regexSource) {
+
+// nachher:
+export function extractLiteralPrefix (regexSource) {
+*/
